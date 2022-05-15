75065462d3ab54450617cf05216e2ac5dbcf2d5d
JeremiasArriondo <jeremiasarriondo98@gmail.com>
2022-05-14 23:51:52 -0300

    Actualizando controlador y cargando en db

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,8 @@
-const { response } = require('express');
+const { response, request } = require('express');
+//Importamos el modelo del usuario
+const User = require('../models/user.model');
+
+const bcryptjs = require('bcryptjs');
 
 const userGet = (req, res = response) => {
     res.json({
@@ -6,13 +10,27 @@ const userGet = (req, res = response) => {
     });
 }
 
-const userPost = (req, res = response) => {
+const userPost = async (req = request, res = response) => {
 
-    const body = req.body;
+    const { name, email, password } = req.body;
+    const user = new User({name, email, password});
 
+    //Verificar si el correo existe
+    const existeEmail = await User.findOne({ email});
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'EL correo ya existe'
+        })
+    }
+    //Encriptar la contraseña
+    //El salt es el tamaño de la encriptacion, por defecto es 10
+    const salt = bcryptjs.genSaltSync();
+    user.password = bcryptjs.hashSync( password, salt);
+    //Guardar en la base de datos
+    await user.save();
     res.json({
         msg: 'Post API - controller',
-        body
+        user
     });
 }
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { userGet, userPost } = require('../controllers/user.controller');
 
 const router = new Router();
 
 router.get('/', userGet);
 
-router.post('/', userPost);
+router.post('/',
+    [
+        check('email', 'El correo no es válido').isEmail(),
+    ],userPost);
 
-
+//EL check valida lo que viene del body
 
 module.exports = router;
